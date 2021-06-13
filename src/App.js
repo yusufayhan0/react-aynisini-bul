c9dc2811f0a7bd4aea933db8f9faa80ac47c3ba4
yusufayhan0 <yusufay.ay1@gmail.com>
2021-06-13 07:44:03 +0300

    zorluk ve coklu oyuncu eklendi

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
   const [hideShow, setHideShow] = useState([0, -1, -1])
   const [historyTrueSquare, setHistoryTrueSquare] = useState([])
   const [image, setImage] = useState()
+  const [settings, setSettings] = useState({
+    gamercount: "1",
+    difficulty: "1"
+  })
+  const [gamerLine, setGamerLine] = useState("1")
+  const [scoreBoard, setScoreBoard] = useState([])
 
   const ref = useRef(1)
   const count = [];
@@ -21,6 +27,11 @@ function App() {
   }, [])
 
   useLayoutEffect(() => {
+    setSettings({
+      gamercount: "1",
+      difficulty: "1"
+    })
+    setGamerLine("1")
     setSquare(count)
   }, [])
 
@@ -37,7 +48,11 @@ function App() {
             setHideShow(value => [value[0] + 1, tempSquare[0], tempSquare[1]])
           }, 700);
         }
+        setTimeout(() => {
+          gamerLine === "1" ? setGamerLine("2") : setGamerLine("1")
+        }, 700);
       }
+
     }
     else {
       setHideShow([0, -1, -1])
@@ -105,19 +120,60 @@ function App() {
     setImage(imageCreate())
   }, [])
 
+  const handleDifficulty = (e) => {
+    setSettings(setting => ({ ...setting, difficulty: e.target.value }))
+
+  }
+
+  useEffect(() => {
+    console.log(settings)
+  }, [settings])
+
+  const handleGamerCount = (e) => {
+    setSettings(setting => ({ ...setting, gamercount: e.target.value }))
+  }
 
   return (
     <div className="app-container">
+
+
 
       <div className="app-game-container">
         {
           getSquare.map((index) => {
 
-            return <Square key={index} indexNo={index} idNo={image[index]} imagepath={imageList[image[index]]} hideShow={hideShow} tempSq={tempSq} />
+            return <Square key={index} gamesettings={settings} indexNo={index} idNo={image[index]} imagepath={imageList[image[index]]} hideShow={hideShow} tempSq={tempSq} />
           })
         }
       </div>
+      <div className="settings-container">
 
+        <div>
+          {
+            settings.gamercount === "1"
+              ? <p>Tek Oyuncu</p>
+              : <p>{gamerLine} Oyuncu</p>
+          }
+
+        </div>
+
+        <p>Zorluk Derecesi</p>
+        <div>
+          <input type="radio" onChange={handleDifficulty} id="difficulty1" name="difficulty" value="3" />
+          <label for="difficulty1">Zor</label>
+          <input type="radio" onChange={handleDifficulty} id="difficulty2" name="difficulty" value="2" />
+          <label for="difficulty2">Orta</label>
+          <input type="radio" onChange={handleDifficulty} id="difficulty3" name="difficulty" value="1" defaultChecked />
+          <label for="difficulty3">Kolay</label>
+        </div>
+        <p>Oyuncu sayısı</p>
+        <div>
+          <input type="radio" id="gamer1" onChange={handleGamerCount} name="gamercount" value="1" defaultChecked />
+          <label for="gamer1">Tek Kişilik</label>
+          <input type="radio" id="gamer2" onChange={handleGamerCount} name="gamercount" value="2" />
+          <label for="gamer2">2 Kişilik</label>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Square.js
+++ src/Square.js
@@ -4,9 +4,13 @@ import React, { useEffect, useState } from 'react'
 let colorsCharacter = "0123456789abcdef"
 function Square(props) {
 
+    //gamercount: "1"
+    //difficulty: "1"
+
+    const { gamesettings } = props
 
     const { hideShow } = props
-    const [getcolor, setColor] = useState()
+    const [getcolor, setColor] = useState("#444")
     const [isShow, setisShow] = useState(false)
     const [change, setChange] = useState(false)
     const [showClass, setShowClass] = useState("square-own-hide")//square-own-hide
@@ -21,20 +25,33 @@ function Square(props) {
     }
 
     useEffect(() => {
-        createColor()
+        //createColor()
     }, [])
-
-
-
 
     useEffect(() => {
 
+        if (gamesettings.difficulty !== "1") {
+            createColor()
+        }
+        else{
+            setColor("#444")
+        }
+
+    }, [gamesettings])
+
+
+    useEffect(() => {
         if (hideShow[1] !== -1 && hideShow[2] !== -1) {
             if (hideShow[1] === props.indexNo || hideShow[2] === props.indexNo) {
                 setShowClass("square-own-hide")
-                createColor()
+                if (gamesettings.difficulty === "2") {
+                    createColor()
+                }
                 setisShow(false)
             }
+        }
+        if (gamesettings.difficulty === "3") {
+            createColor()
         }
     }, [hideShow])
 
@@ -54,7 +71,7 @@ function Square(props) {
             props.tempSq(props.indexNo, props.idNo)
         }}>
             <div className={"square-own-container " + showClass}>
-                <div className="common div-rotate" style={{ backgroundColor: getcolor }}></div>
+                <div className="common div-rotate" style={{ backgroundColor: getcolor,transition:"300ms all" }}></div>
                 <div className="common img-rotate">
                     <img src={"../images/" + props.imagepath} alt="" width="100%" height="100%" />
                 </div>
